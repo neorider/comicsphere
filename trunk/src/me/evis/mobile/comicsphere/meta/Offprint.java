7e1b96f46b65da95af944c74fa36755eb7898bbd
EviSong <EviSong@495546e9-5545-39c5-a919-8358f6ff0694>
2011-05-15 10:43:34 +0000

    Android ver.

---
+++ trunk/src/me/evis/mobile/comicsphere/meta/Offprint.java
@@ -0,0 +1,10 @@
+package me.evis.mobile.comicsphere.meta;
+
+public class Offprint extends Edition {
+    private static final String OFFPRINT = "offprint";
+
+    @Override
+    public String getEditionType() {
+        return OFFPRINT;
+    }
+}
